8edfd63db17b2f2c20cbc2fc657e4022ef90d4fc
Keshav Sharma <46739493+sharmakeshav10@users.noreply.github.com>
2022-07-26 10:06:14 +0000

    Fibonacci sequence algorithm

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+/*
+  Given a number 'n', find the first 'n' elements of the fibonacci sequence.
+  fibonacci[0] = 0 and fibonacci[1] = 1
+  fibonacci[2] = [0, 1]
+  fibonacci[4] = [0, 1, 1, 2] 
+  ... and so on.
+*/
+
+function fibonacci(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fibonacci(3)) //[0, 1, 1]
+console.log(fibonacci(8)); //[0, 1, 1, 2, 3, 5, 8, 13]
