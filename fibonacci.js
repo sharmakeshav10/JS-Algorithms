b192591ad9c947a0ea2fc7a6567dad8aa21783dc
Keshav Sharma <46739493+sharmakeshav10@users.noreply.github.com>
2022-07-28 17:21:02 +0000

    recursive fibonacci approach

--- fibonacci.js
+++ fibonacci.js
@@ -6,13 +6,26 @@
   ... and so on.
 */
 
-function fibonacci(n) {
-  const fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+// function fibonacci(n) {
+//   const fib = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     fib[i] = fib[i - 1] + fib[i - 2];
+//   }
+//   return fib;
+// }
+
+// console.log(fibonacci(3)) //[0, 1, 1]
+// console.log(fibonacci(8)); //[0, 1, 1, 2, 3, 5, 8, 13]
+
+
+/* Recursive approach for fibonacci */
+function recFibonacci(n) {
+  if(n < 2) {
+    return n;
   }
-  return fib;
+
+  return recFibonacci(n-1) + recFibonacci(n-2);
 }
 
-console.log(fibonacci(3)) //[0, 1, 1]
-console.log(fibonacci(8)); //[0, 1, 1, 2, 3, 5, 8, 13]
+console.log(recFibonacci(6)); //8
+console.log(recFibonacci(8)); //21
